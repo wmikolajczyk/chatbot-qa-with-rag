a6a2c1dd620871f1af5eee00b70ef65a8abb5401
wmikolajczyk <mikolajczyk.wojtas@gmail.com>
2024-01-17 19:29:06 +0100

    Add sim search

--- chatbot_qa.py
+++ chatbot_qa.py
@@ -5,12 +5,17 @@ MODEL_ID = "hf-e5"
 VECTOR_DB_ID = "faiss"
 
 
-def main():
+def similarity_search(query):
     embedding_model = get_embedding_model(MODEL_ID)
     vector_db = get_vector_db(VECTOR_DB_ID, embedding_model, VECTOR_DB_PATH)
 
-    query = "Top Gun"
     relevant_docs = vector_db.similarity_search(query)
+    return relevant_docs
+
+
+def main():
+    query = "Top Gun"
+    relevant_docs = similarity_search(query)
     print(relevant_docs)
 
 
